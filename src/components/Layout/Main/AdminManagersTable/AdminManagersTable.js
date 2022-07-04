4202074a60ee7d44e127f80ea6701fe573cc810b
Dana <danaz.radetskaya@gmail.com>
2022-07-04 15:48:19 +0300

    added updating and deleting managers

--- src/components/Layout/Main/AdminManagersTable/AdminManagersTable.js
+++ src/components/Layout/Main/AdminManagersTable/AdminManagersTable.js
@@ -2,18 +2,32 @@ import React, { useState } from 'react';
 import axios from "axios";
 import {TabulatorFull as Tabulator} from "tabulator-tables";
 
-import './AdminManagersTable.scss'
+import './AdminManagersTable.scss';
+import {useNavigate} from "react-router";
+import {Link} from "react-router-dom";
 
 export function AdminManagersTable() {
     const [managers, setManagers] = useState([]);
+    let table;
 
-    const tabledata = [
-        {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
-        {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
-        {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
-        {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
-        {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
-    ];
+    const data = managers.map(currentValue => {
+        let obj = {
+            id:currentValue.managerId,
+            fio:currentValue.lastName + ' ' + currentValue.firstName + ' ' + currentValue.patronymic,
+            login:currentValue.login,
+            experience:currentValue.workExperience,
+            phoneNumber:currentValue.phone
+        }
+        return obj;
+    })
+    const tabledata = data;
+
+    let deleteItem = function () {
+        return "Удалить";
+    };
+    let updateItem = function () {
+        return "Изменить";
+    };
 
     React.useEffect(() => {
         axios.get('https://localhost:7274/api/managers')
@@ -22,18 +36,46 @@ export function AdminManagersTable() {
                 console.log(res.data)
             })
 
-        let table = new Tabulator("#manager-table", {
+        table = new Tabulator("#manager-table", {
             height:"311px",
             data:tabledata,
             layout:"fitColumns", //fit columns to width of table (optional)
             columns:[ //Define Table Columns
-                {title:"Name", field:"name", width:150},
-                {title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
-                {title:"Favourite Color", field:"col", formatter: "color"},
-                {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+                {title:"ФИО", field:"fio", width:200, editor: "input"},
+                {title:"Логин", field:"login", hozAlign:"left", editor: "input"},
+                {title:"Опыт работы", field:"experience", formatter: "progress", editor: "input"},
+                {title:"Телефон", field:"phoneNumber", hozAlign:"center", editor: "input"},
+                {title:"Удалить", hozAlign:"center", formatter: deleteItem,
+                    cellClick: function (e, cell) {
+                        cell.getRow().delete();
+                        axios.delete(`https://localhost:7274/api/managers/${cell.getRow().getData().id}`)
+                             .then(response => console.log(response.data))
+                    }},
+                {title:"Изменить", hozAlign:"center", formatter: updateItem,
+                    cellClick: function (e, cell) {
+                        console.log("update=>" + cell.getRow().getData().id);
+                        cell.getRow().update();
+                        let fioArray = cell.getRow().getData().fio.split(" ");
+                        axios.put(`https://localhost:7274/api/users/${cell.getRow().getData().id}`, {
+                            "firstName": fioArray[0],
+                            "lastName": fioArray[1],
+                            "patronymic": fioArray[2],
+                            "phone": cell.getRow().getData().phoneNumber,
+                            "login": cell.getRow().getData().login
+                        })
+                            .then(response => {
+                                console.log(response.data)
+                                axios.put(`https://localhost:7274/api/managers/${cell.getRow().getData().id}`, {
+                                    "workExperience": cell.getRow().getData().experience
+                                })
+                                    .then(response => console.log(response.data))
+                            })
+                    }}
+                /*{title:"Удалить", field:"delete", hozAlign:"center"}*/
             ],
+
         });
-    }, []);
+    }, [managers.length]);
 
     return (
         <div>
