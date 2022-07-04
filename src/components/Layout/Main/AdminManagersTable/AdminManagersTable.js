02be9cdaea7337ff40b5f2f05146ccf2d9e2097c
Dana <danaz.radetskaya@gmail.com>
2022-07-04 16:11:52 +0300

    made some edits

--- src/components/Layout/Main/AdminManagersTable/AdminManagersTable.js
+++ src/components/Layout/Main/AdminManagersTable/AdminManagersTable.js
@@ -3,8 +3,6 @@ import axios from "axios";
 import {TabulatorFull as Tabulator} from "tabulator-tables";
 
 import './AdminManagersTable.scss';
-import {useNavigate} from "react-router";
-import {Link} from "react-router-dom";
 
 export function AdminManagersTable() {
     const [managers, setManagers] = useState([]);
@@ -47,14 +45,12 @@ export function AdminManagersTable() {
                 {title:"Телефон", field:"phoneNumber", hozAlign:"center", editor: "input"},
                 {title:"Удалить", hozAlign:"center", formatter: deleteItem,
                     cellClick: function (e, cell) {
-                        cell.getRow().delete();
                         axios.delete(`https://localhost:7274/api/managers/${cell.getRow().getData().id}`)
-                             .then(response => console.log(response.data))
+                             .then(response => cell.getRow().delete())
                     }},
                 {title:"Изменить", hozAlign:"center", formatter: updateItem,
                     cellClick: function (e, cell) {
                         console.log("update=>" + cell.getRow().getData().id);
-                        cell.getRow().update();
                         let fioArray = cell.getRow().getData().fio.split(" ");
                         axios.put(`https://localhost:7274/api/users/${cell.getRow().getData().id}`, {
                             "firstName": fioArray[0],
@@ -64,11 +60,10 @@ export function AdminManagersTable() {
                             "login": cell.getRow().getData().login
                         })
                             .then(response => {
-                                console.log(response.data)
                                 axios.put(`https://localhost:7274/api/managers/${cell.getRow().getData().id}`, {
                                     "workExperience": cell.getRow().getData().experience
                                 })
-                                    .then(response => console.log(response.data))
+                                    .then(response =>  cell.getRow().update())
                             })
                     }}
                 /*{title:"Удалить", field:"delete", hozAlign:"center"}*/
