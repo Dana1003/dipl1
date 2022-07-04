e7d7f716b8702df01d1c16b1273c6d35e8e0efb0
Dana <danaz.radetskaya@gmail.com>
2022-07-04 12:05:06 +0300

    added new component 'AdminManagersTable'

---
+++ src/pages/AdminViewManagerDetails/AdminViewManagerDetails.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { AdminHeader } from "../../components/Layout/Header/AdminHeader";
+import { AdminManagersTable } from "../../components/Layout/Main/AdminManagersTable/AdminManagersTable";
+
+export function AdminViewManagerDetails() {
+    return (
+        <div>
+            <AdminHeader />
+            <AdminManagersTable />
+        </div>
+    );
+}
+
+
