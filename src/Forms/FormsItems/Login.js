fc01b707306a82987bcbac825bee5b47dea82540
Dana <danaz.radetskaya@gmail.com>
2022-07-07 12:51:51 +0300

    added form component 'login'

---
+++ src/Forms/FormsItems/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import {Form, Input} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+
+export function Login() {
+    const [login, setLogin] = useState('');
+
+    return (
+        <>
+            <Form.Item
+                label="Логин"
+                name="login"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Вы не ввели логин!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Введите логин"
+                    onChange={(e) => setLogin(e.target.value)}
+                />
+            </Form.Item>
+        </>
+    );
+};
