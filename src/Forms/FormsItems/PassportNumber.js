92f68bbd189153e419b22eb06fd1eb8bf9a00fe3
Dana <danaz.radetskaya@gmail.com>
2022-07-07 13:14:36 +0300

    created form component 'passportNumber'

---
+++ src/Forms/FormsItems/PassportNumber.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Form, Input } from "antd";
+
+export function PassportNumber() {
+    const [setPassportNumber] = useState('');
+
+    return (
+        <Form.Item
+            label="Номер паспорта"
+            name="passportNumber"
+            rules={[
+                {
+                    required: true,
+                    message: 'Вы не ввели номер паспорта!',
+                },
+            ]}
+        >
+            <Input
+                placeholder="Введите номер паспорта"
+                onChange={(e) => setPassportNumber(e.target.value)}
+            />
+        </Form.Item>
+    );
+}
