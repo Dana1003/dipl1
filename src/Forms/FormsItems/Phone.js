1310de24be93e1852dee8b3980ced0055996a4de
Dana <danaz.radetskaya@gmail.com>
2022-07-07 13:08:07 +0300

    made some edits

--- src/Forms/FormsItems/Phone.js
+++ src/Forms/FormsItems/Phone.js
@@ -1,28 +1,25 @@
 import React, {useState} from 'react';
+
 import {Form, Input} from "antd";
-import {LockOutlined} from "@ant-design/icons";
 
 export function Phone() {
     const [setPhone] = useState('');
 
     return (
-        <>
-            <Form.Item
-                label="Номер телефона"
-                name="phone"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Вы не ввели номер телефона!',
-                    },
-                ]}
-            >
-                <Input.Password
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    placeholder="Введите номер телефона"
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-            </Form.Item>
-        </>
+        <Form.Item
+            label="Номер телефона"
+            name="phone"
+            rules={[
+                {
+                    required: true,
+                    message: 'Вы не ввели номер телефона!',
+                },
+            ]}
+        >
+            <Input
+                placeholder="Введите номер телефона"
+                onChange={(e) => setPhone(e.target.value)}
+            />
+        </Form.Item>
     );
 }
