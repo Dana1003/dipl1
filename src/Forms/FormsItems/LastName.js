609cd81780d0c90c2030ea19bb1c4773b047c26e
Dana <danaz.radetskaya@gmail.com>
2022-07-07 12:58:45 +0300

    created form component 'lastName'

---
+++ src/Forms/FormsItems/LastName.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import {Form, Input} from "antd";
+
+export function LastName() {
+    const [setLastName] = useState('');
+
+    return (
+        <>
+            <Form.Item
+                label="Фамилия"
+                name="lastName"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Вы не ввели фамилию!',
+                    },
+                ]}
+            >
+                <Input
+                    placeholder="Введите фамилию"
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </Form.Item>
+        </>
+    );
+}
