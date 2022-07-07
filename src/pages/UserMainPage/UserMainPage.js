9c3babd632183a2286ea566d5806d10cf704424c
Dana <danaz.radetskaya@gmail.com>
2022-07-07 11:43:51 +0300

    created user main page and added user header

---
+++ src/pages/UserMainPage/UserMainPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UserHeader } from "../../components/Layout/Header/UserHeader";
+
+export function UserMainPage() {
+    return (
+        <>
+            <UserHeader />
+        </>
+    );
+};
