8122d1e1de216d26dba53ea48bee6e6cb121256d
Dana <danaz.radetskaya@gmail.com>
2022-07-07 16:47:24 +0300

    added main block to user page

--- src/pages/UserMainPage/UserMainPage.js
+++ src/pages/UserMainPage/UserMainPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import { UserHeader } from "../../components/Layout/Header/UserHeader";
+import { MainPageBlock } from "../../components/Layout/Main/MainPageBlock";
 
 export function UserMainPage() {
     return (
         <>
             <UserHeader />
+            <MainPageBlock />
         </>
     );
 }
