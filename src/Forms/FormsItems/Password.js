5e00b5cc392c46db146fb1bada9dea55585e7581
Dana <danaz.radetskaya@gmail.com>
2022-07-07 12:52:10 +0300

    added form component 'password'

---
+++ src/Forms/FormsItems/Password.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import {Form, Input} from "antd";
+import {LockOutlined} from "@ant-design/icons";
+
+export function Password() {
+    const [password, setPassword] = useState('');
+
+    return (
+        <>
+            <Form.Item
+                label="Пароль"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Вы не ввели пароль!',
+                    },
+                ]}
+            >
+                <Input.Password
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    placeholder="Введите пароль"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </Form.Item>
+        </>
+    );
+};
