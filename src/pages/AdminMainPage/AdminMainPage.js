79d35848bbfc86f4cfc18c4cb712134a032f62ee
Dana <danaz.radetskaya@gmail.com>
2022-07-13 13:31:37 +0300

    made some edits

--- src/pages/AdminMainPage/AdminMainPage.js
+++ src/pages/AdminMainPage/AdminMainPage.js
@@ -1,30 +1,13 @@
 import React from 'react';
+
 import { AdminHeader } from "../../components/Layout/Header/AdminHeader";
-import { useState } from "react";
-import axios from "axios";
+import { MainPageBlock } from "../../components/Layout/Main/MainPageBlock";
 
 export function AdminMainPage() {
-    const [lastName, setLastName] = useState('');
-
-/*    function findHandler() {
-        axios.get(`https://localhost:7274/api/managers/findManagers?LastName=${lastName}`)
-            .then(response => {
-                setLastName(response.data)
-                console.log(response.data)
-            })
-    }*/
-
     return (
         <div>
             <AdminHeader />
-{/*            <div>
-                <input className="test-input"
-                       type="text"
-                       id="lastName"
-                       placeholder="Введите фамилию"
-                       onChange={(e) => setLastName(e.target.value)}/>
-                <button className="test-input transition" onClick={findHandler}>Сохранить</button>
-            </div>*/}
+            <MainPageBlock />
         </div>
     );
 }
