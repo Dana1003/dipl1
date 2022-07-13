d24377e31354544ed0be56ecd12b95c4f4a864ec
Dana <danaz.radetskaya@gmail.com>
2022-07-13 15:35:46 +0300

    created new page for user with reviews

---
+++ src/pages/UserReviewPage/UserReviewPage.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+import { UserHeader } from "../../components/Layout/Header/UserHeader";
+import { Reviews } from "../../components/Layout/Main/MainReviews/Reviews";
+import { Pagination } from "../../components/Layout/Main/Pagination";
+
+export function UserReviewPage() {
+    const [reviewsCount, setReviewsCount] = useState(0);
+    const [reviewsText, setReviewsText] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [reviewsPerPage] = useState(5);
+
+    useEffect(() => {
+        axios.get(`https://localhost:7274/api/reviews/pagingReviews?PageNumber=${currentPage}&PageSize=${reviewsPerPage}`)
+            .then(res => {
+                const {countOfElements, reviewPaginations} = res.data;
+
+                setReviewsText(reviewPaginations);
+                setReviewsCount(countOfElements);
+            })
+
+    }, [currentPage]);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+        <>
+            <UserHeader />
+            <Reviews reviews={reviewsText}/>
+            <Pagination reviewsPerPage={reviewsPerPage} totalReviews={reviewsCount} paginate={paginate}/>
+        </>
+    );
+}
