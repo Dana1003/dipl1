bd6e60dbc31332f5ef6583d8c9c2c25e36baa50c
Dana <danaz.radetskaya@gmail.com>
2022-07-14 14:17:45 +0300

    relocate file

--- src/components/Layout/Main/MainReviews/Pagination.js
+++ src/components/Layout/Main/MainReviews/Pagination.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-import './MainReviews/ReviewPagination.scss';
+import './ReviewPagination.scss';
 
 export const Pagination = ({reviewsPerPage, totalReviews, paginate, link}) => {
     const pageNumber = [];
