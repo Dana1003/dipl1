c15a2196b905c125602413a46f58db51ac64dabc
Dana <danaz.radetskaya@gmail.com>
2022-07-14 13:29:14 +0300

    created component 'textarea'

---
+++ src/Forms/FormsItems/ReviewTextarea.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Form, Input } from "antd";
+const { TextArea } = Input;
+
+export function ReviewTextarea({onChange}) {
+    return (
+        <Form.Item
+            label="Текст отзыва"
+            rules={[
+                {
+                    required: true,
+                    message: 'Вы не ввели отзыв!',
+                },
+            ]}
+        >
+            <TextArea
+                showCount
+                maxLength={500}
+                style={{
+                    height: 300,
+                }}
+                placeholder="Введите свой отзыв"
+                onChange={ onChange }
+            />
+        </Form.Item>
+    );
+}
