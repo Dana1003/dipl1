64428787f27ac79bb54b86e23d875c4a5a35062b
Dana <danaz.radetskaya@gmail.com>
2022-07-14 13:24:48 +0300

    made some edits

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <App/>
-        </Router>
-    </React.StrictMode>
+    <Router>
+        <App/>
+    </Router>
 );
 
