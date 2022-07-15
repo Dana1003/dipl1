a0d9c16676852197e44412ac69be2fecb7aa9463
Dana <danaz.radetskaya@gmail.com>
2022-07-15 13:19:52 +0300

    made some edits

--- src/Forms/FormsItems/Login.js
+++ src/Forms/FormsItems/Login.js
@@ -3,23 +3,23 @@ import React from 'react';
 import { Form, Input } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
-export function Login({ onChange, value }) {
+export function Login({onChange, value}) {
     return (
-            <Form.Item
-                label="Логин"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Вы не ввели логин!',
-                    },
-                ]}
-            >
-                <Input
-                    value={value}
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder="Введите логин"
-                    onChange={onChange}
-                />
-            </Form.Item>
+        <Form.Item
+            label="Логин"
+            rules={[
+                {
+                    required: true,
+                    message: 'Вы не ввели логин!',
+                },
+            ]}
+        >
+            <Input
+                value={value}
+                prefix={<UserOutlined className="site-form-item-icon"/>}
+                placeholder="Введите логин"
+                onChange={onChange}
+            />
+        </Form.Item>
     );
 }
