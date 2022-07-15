b164ebc0c4d2591f37e3ea4194076c5a64dbb80b
Dana <danaz.radetskaya@gmail.com>
2022-07-15 14:03:15 +0300

    created select for arrival city

---
+++ src/Forms/FormsItems/ArrivalCity.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Form, Select } from "antd";
+const { Option } = Select;
+
+export function ArrivalCity({onChange}) {
+    return (
+        <Form.Item
+            label="Город отправления"
+            rules={[
+                {
+                    required: true,
+                    message: 'Вы не выбрали город отправления!',
+                },
+            ]}
+        >
+            <Select
+                showSearch
+                placeholder="Выберите город отправления"
+                optionFilterProp="children"
+                onChange={onChange}
+                onSearch
+                filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+            >
+                <Option value="Минск">Минск</Option>
+                <Option value="Гомель">Гомель</Option>
+                <Option value="Витебск">Витебск</Option>
+            </Select>
+        </Form.Item>
+    );
+}
