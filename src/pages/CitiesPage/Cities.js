4b28ae2db87bf1c77f29b0f02ca21be8375583db
Dana <danaz.radetskaya@gmail.com>
2022-07-18 16:57:52 +0300

    added Footer

--- src/pages/CitiesPage/Cities.js
+++ src/pages/CitiesPage/Cities.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { MainHeader } from "../../components/Layout/Header/MainHeader";
 import { MainCities } from "../../components/Layout/Main/MainCIties/MainCities";
+import { Footer } from "../../components/Layout/Footer/Footer";
 
 export function Cities() {
     return (
         <>
             <MainHeader/>
             <MainCities />
+            <Footer />
         </>
     );
 }
--- src/pages/KindsOfTourism/KindsOfTourism.js
+++ src/pages/KindsOfTourism/KindsOfTourism.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import {MainHeader} from "../../components/Layout/Header/MainHeader";
 import {MainKindsOfTourism} from "../../components/Layout/Main/MainKindsOfTourism/MainKindsOfTourism";
+import { Footer } from "../../components/Layout/Footer/Footer";
 
 export function KindsOfTourism() {
     return (
-        <div>
+        <>
             <MainHeader/>
             <MainKindsOfTourism />
-        </div>
+            <Footer />
+        </>
     );
-};
+}
