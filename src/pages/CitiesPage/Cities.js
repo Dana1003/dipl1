f438534b3e9e6167f363d7b2744d5b3e1c513949
Dana <danaz.radetskaya@gmail.com>
2022-07-18 16:20:39 +0300

    created page with cities of Belarus

---
+++ src/pages/CitiesPage/Cities.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MainHeader } from "../../components/Layout/Header/MainHeader";
+import { MainCities } from "../../components/Layout/Main/MainCIties/MainCities";
+
+export function Cities() {
+    return (
+        <>
+            <MainHeader/>
+            <MainCities />
+        </>
+    );
+}
