4491ed6cb9d2508f30bfd42317a0f867da365337
Dana <danaz.radetskaya@gmail.com>
2022-07-20 16:27:08 +0300

    made some edits

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </Router>
 );
