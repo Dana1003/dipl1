aa29a1ee8eaaab8a47daf2c5602e59080f0e10a6
aria.nik@mail.ru <aria.nik@mail.ru>
2022-07-21 15:10:36 +0300

    добавил диаграмму с популярными турами

--- src/components/Layout/Main/UserTheMostPopularTours/UserTheMostPopularTours.js
+++ src/components/Layout/Main/UserTheMostPopularTours/UserTheMostPopularTours.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import CanvasJSReact from '../../../../lib/canvasjs.react';
+let CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export function UserTheMostPopularTours() {
     const [tickets, setTickets] = useState([]);
@@ -9,22 +11,43 @@ export function UserTheMostPopularTours() {
         axios.get('https://localhost:7274/api/tickets')
             .then(res => {
                 setTickets(res.data.map(x => x.nameOfTour));
-
             });
     }, []);
 
-    useEffect(() => {
-        if(tickets.length != 0) {
-            tickets.forEach(function (a) {
-                dataToChart[a] = dataToChart[a] + 1 || 1;
-            });
-            console.log(dataToChart)
-        }
-    }, [tickets])
+    let obj = [];
+
+    tickets.forEach(function (a) {
+        dataToChart[a] = dataToChart[a] + 1 || 1;
+    });
+    for (let value in dataToChart) {
+        obj.push({
+            y: dataToChart[value],
+            label: value
+        })
+    }
+
+    const options = {
+        exportEnabled: true,
+        animationEnabled: true,
+        title: {
+            text: "Популярные туры среди клиентов"
+        },
+        data: [{
+            type: "pie",
+            startAngle: 75,
+            toolTipContent: "<b>{label}</b>: {y}",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y}",
+            dataPoints: obj
+        }]
+    }
 
     return (
         <div>
-            <h1>Тут должна быть диагармма :)</h1>
+            <CanvasJSChart options={options}
+            />
         </div>
     );
 }
--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { MainHeader } from "../../components/Layout/Header/MainHeader";
-import { Maps } from "../../components/Maps/Maps";
 import { Footer } from "../../components/Layout/Footer/Footer";
 
 export function AboutUs() {
     return (
         <>
             <MainHeader />
-            <Maps />
             <Footer />
         </>
     );
