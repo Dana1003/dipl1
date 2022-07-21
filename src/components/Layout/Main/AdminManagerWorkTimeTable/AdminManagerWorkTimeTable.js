93bddecdb56c95cb24b8d65f3b7361c2aa6b784b
aria.nik@mail.ru <aria.nik@mail.ru>
2022-07-21 16:26:08 +0300

    добавил просмотр отработанного расписания

--- src/components/Layout/Main/AdminManagerWorkTimeTable/AdminManagerWorkTimeTable.js
+++ src/components/Layout/Main/AdminManagerWorkTimeTable/AdminManagerWorkTimeTable.js
@@ -1,9 +1,67 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import {DatePicker, Table} from "antd";
+import moment from "moment";
 
 export function AdminManagerWorkTimeTable() {
+    const [workedTime, setWorkedTime] = useState([])
+
+    React.useEffect(() => {
+        axios.get('https://localhost:7274/api/workedHour')
+            .then(res => {
+                setWorkedTime(res.data);
+            });
+    }, []);
+    React.useEffect(() => {
+    }, [workedTime]);
+
+    const columns = [
+        {
+            title: 'Фамилия',
+            dataIndex: 'lastName',
+            sorter: (a, b) => a.lastName.length - b.lastName.length,
+        },
+        {
+            title: 'Имя',
+            dataIndex: 'firstName',
+        },
+        {
+            title: 'Дата входа в систему',
+            dataIndex: 'startDate',
+        },
+        {
+            title: 'Дата выхода из системы',
+            dataIndex: 'endDate',
+        }
+    ];
+
     return (
-        <div>
-            <h1>Таблица отработанного времени</h1>
+        <div className="manager-table">
+            <Table columns={columns}
+                   title={() => 'Отработанные часы'}
+                   pagination={{pageSize: 5}}
+                   dataSource={workedTime.map(currentValue => ({
+                       key: currentValue.workedHourId,
+                       managerId: currentValue.managerId,
+                       firstName: currentValue.firstName,
+                       lastName: currentValue.lastName,
+                       startDate: <DatePicker
+                           value={moment(currentValue.startDate)}
+                           showTime={{
+                               defaultValue: moment((currentValue.startDate), 'HH:mm'),
+                               format: ('HH:mm')
+                           }}
+                           disabled={true}
+                       />,
+                       endDate: <DatePicker
+                           value={moment(currentValue.endDate)}
+                           showTime={{
+                               defaultValue: moment((currentValue.endDate), 'HH:mm'),
+                               format: ('HH:mm')
+                           }}
+                           disabled={true}
+                       />
+                   }))}/>
         </div>
     );
 }
