389c2c5d819caac3d78270713bd1b6d087cd38f7
Dana <danaz.radetskaya@gmail.com>
2022-07-25 16:03:51 +0300

    added footer

--- src/pages/TouristMemoPage/TouristMemoPage.js
+++ src/pages/TouristMemoPage/TouristMemoPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { MainHeader } from "../../components/Layout/Header/MainHeader";
 import { TouristMemo } from "../../components/Layout/Main/TouristMemo/TouristMemo";
+import { Footer } from "../../components/Layout/Footer/Footer";
 
 export function TouristMemoPage() {
     return (
         <>
             <MainHeader />
             <TouristMemo />
+            <Footer />
         </>
     );
 }
--- src/pages/UserTouristMemoPage/UserTouristMemoPage.js
+++ src/pages/UserTouristMemoPage/UserTouristMemoPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { UserHeader } from "../../components/Layout/Header/UserHeader";
 import { TouristMemo } from "../../components/Layout/Main/TouristMemo/TouristMemo";
+import { Footer } from "../../components/Layout/Footer/Footer";
 
 export function UserTouristMemoPage() {
     return (
         <>
             <UserHeader />
             <TouristMemo />
+            <Footer />
         </>
     );
 }
