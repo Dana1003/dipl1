9b73d9483a8c35a87baf38a804efd2739f6239cf
aria.nik@mail.ru <aria.nik@mail.ru>
2022-07-26 14:15:09 +0300

    добавил возможность редактирования личных данных

--- src/Forms/PrivateDataForm/PrivateDataForm.js
+++ src/Forms/PrivateDataForm/PrivateDataForm.js
@@ -1,52 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Password } from "../FormsItems/Password";
-import { Login } from "../FormsItems/Login";
-import { LastName } from "../FormsItems/LastName";
-import { FirstName } from "../FormsItems/FirstName";
-import { Patronymic } from "../FormsItems/Patronymic";
-import { Phone } from "../FormsItems/Phone";
-import { PassportNumber } from "../FormsItems/PassportNumber";
-import { Email } from "../FormsItems/Email";
-import { SaveButton } from "../FormsItems/SaveButton";
-import { BirthdayDatePicker } from "../FormsItems/BirthdayDatePicker";
+import React, {useState, useEffect} from 'react';
+import {Password} from "../FormsItems/Password";
+import {Login} from "../FormsItems/Login";
+import {LastName} from "../FormsItems/LastName";
+import {FirstName} from "../FormsItems/FirstName";
+import {Patronymic} from "../FormsItems/Patronymic";
+import {Phone} from "../FormsItems/Phone";
+import {PassportNumber} from "../FormsItems/PassportNumber";
+import {Email} from "../FormsItems/Email";
+import {SaveButton} from "../FormsItems/SaveButton";
 
-import {DatePicker, Form} from "antd";
+import {DatePicker, Form, notification} from "antd";
 
 import './PrivateDataForm.scss';
 import axios from "axios";
 import moment from "moment";
+import {CloseCircleOutlined, CheckCircleOutlined} from "@ant-design/icons";
 
 export function PrivateDataForm() {
-    const [client, setClient] = useState(null);
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [patronymic, setPatronymic] = useState('');
-    const [phone, setPhone] = useState('');
-    const [email, setEmail] = useState('');
-    const [passportNumber, setPassportNumber] = useState('');
-    const [birthDate, setBirthDate] = useState(null);
+    const [client, setClient] = useState('');
 
     useEffect(() => {
         axios.get(`https://localhost:7274/api/clients/${1}`)
             .then(res => {
                 setClient(res.data)
-
-                setLogin(res.data.login);
-                setPassword(res.data.password)
-                setLastName(res.data.lastName)
-                setFirstName(res.data.firstName)
-                setPatronymic(res.data.patronymic)
-                setPhone(res.data.phone)
-                setEmail(res.data.email)
-                setPassportNumber(res.data.passportNumber)
-                setBirthDate(moment(res.data.bithDate))
             });
     }, []);
 
-    const onUpdateClientData = () =>{
+    useEffect(() => {
+    }, [client])
 
+    function errorNotification() {
+        notification.open({
+            message: 'Ошибка изменения личных данных!',
+            icon: <CloseCircleOutlined style={{color: "red"}}/>
+        });
+    }
+
+    function successNotification() {
+        notification.open({
+            message: 'Данные успешно изменены!',
+            icon: <CheckCircleOutlined style={{color: "green"}}/>
+        });
+    }
+
+    const onUpdateClientData = () => {
+        axios.put(`https://localhost:7274/api/clients/clientUser/${client.clientId}`, ({
+            "clientId": client.clientId,
+            "passportNumber": client.passportNumber,
+            "email": client.email,
+            "bithDate": moment(client.bithDate).utcOffset('GMT').format(),
+            "userId": client.userId,
+            "firstName": client.firstName,
+            "lastName": client.lastName,
+            "patronymic": client.patronymic,
+            "phone": client.phone,
+            "login": client.login,
+            "password": client.password,
+            "role": "User"
+        })).then(temp => {
+            axios.get(`https://localhost:7274/api/clients/${client.clientId}`)
+                .then(res => {
+                    setClient(res.data);
+                    successNotification()
+                });
+        }).catch(err => {
+            if (err.response.status === 500) {
+                errorNotification()
+            }
+        })
     }
 
     let clientData = [];
@@ -56,8 +77,6 @@ export function PrivateDataForm() {
             value: client[field]
         })
     }
-
-    console.log(clientData)
 
     return (
         <div className="form">
@@ -77,27 +96,61 @@ export function PrivateDataForm() {
                 }}
                 autoComplete="off"
             >
-                <Login onChange={(login) => {setLogin(login)}}/>
-                <Password onChange={(password) => {setPassword(password)}}/>
-                <LastName onChange={(lastName) => {setLastName(lastName)}}/>
-                <FirstName onChange={(firstName) => {setFirstName(firstName)}}/>
-                <Patronymic onChange={(patronymic) => {setPatronymic(patronymic)}}/>
-                <Phone onChange={(phone) => {setPhone(phone)}}/>
+                <Login onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, login: e.target.value}
+                    })
+                }}/>
+                <Password onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, password: e.target.value}
+                    })
+                }}/>
+                <LastName onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, lastName: e.target.value}
+                    })
+                }}/>
+                <FirstName onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, firstName: e.target.value}
+                    })
+                }}/>
+                <Patronymic onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, patronymic: e.target.value}
+                    })
+                }}/>
+                <Phone onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, phone: e.target.value}
+                    })
+                }}/>
                 <DatePicker
                     required={true}
                     allowClear={false}
-                    value={moment(birthDate)}
+                    value={moment(client.bithDate)}
                     showTime={{
-                        value: moment(birthDate),
+                        value: moment(client.bithDate),
                         format: ('HH:mm')
                     }}
-                    onChange={(value)=>{
-                        setBirthDate(value)
+                    onChange={(e) => {
+                        setClient(pre => {
+                            return {...pre, bithDate: e}
+                        })
                     }}
                 />
-                <Email onChange={(email) => {setEmail(email)}}/>
-                <PassportNumber onChange={(passportNumber) => {setPassportNumber(passportNumber)}}/>
-                <SaveButton />
+                <Email onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, email: e.target.value}
+                    })
+                }}/>
+                <PassportNumber onChange={(e) => {
+                    setClient(pre => {
+                        return {...pre, passportNumber: e.target.value}
+                    })
+                }}/>
+                <SaveButton/>
             </Form>
         </div>
     );
