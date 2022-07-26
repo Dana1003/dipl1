8bafaf9abe41d3f80bef40f452c11376f9fbdbf2
Dana <danaz.radetskaya@gmail.com>
2022-07-26 15:05:39 +0300

    created main page for manager

---
+++ src/pages/ManagerMainPage/ManagerMainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ManagerHeader } from "../../components/Layout/Header/ManagerHeader";
+import { MainPageBlock } from "../../components/Layout/Main/MainPageBlock";
+
+export function ManagerMainPage() {
+    return (
+        <>
+            <ManagerHeader />
+            <MainPageBlock />
+        </>
+    );
+}
