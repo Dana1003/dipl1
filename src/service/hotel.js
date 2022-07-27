4fb93fc31d32c4a1900231a080b9f5ca8bf39411
Dana <danaz.radetskaya@gmail.com>
2022-07-27 13:29:11 +0300

    created new file with hotel service

---
+++ src/service/hotel.js
@@ -0,0 +1,79 @@
+import $api from '../http';
+import { ROUTS_API } from "../routs";
+
+import { notification } from "antd";
+import { CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+
+function successDeleteNotification() {
+    notification.open({
+        message: 'Данные успешно удалены!',
+        icon: <DeleteOutlined style={{color: "green"}}/>
+    })
+}
+
+function successAddNotification() {
+    notification.open({
+        message: 'Данные успешно добавлены!',
+        icon: <CheckCircleOutlined style={{color: "green"}}/>
+    })
+}
+
+function errorNotification() {
+    notification.open({
+        message: 'Что-то пошло не так!',
+        icon: <CloseCircleOutlined style={{color: "red"}}/>
+    });
+}
+
+const HotelService = {
+    getHotels(setHotels) {
+        return $api.get(ROUTS_API.hotels)
+            .then(response => {
+                setHotels(response.data)
+            })
+    },
+    deleteHotel(key, hotels, setHotels) {
+        return $api.delete(ROUTS_API.hotels + `/${key}`)
+            .then(response => {
+                setHotels(hotels.filter((item) => item.hotelId !== key))
+                successDeleteNotification()
+            })
+            .catch(error => {
+                console.log(error.message)
+                errorNotification()
+            })
+    },
+    postHotel(hotelToAdd, hotels, setHotels) {
+        return $api.post(ROUTS_API.hotels, hotelToAdd)
+            .then(response => {
+                setHotels([...hotels, response.data])
+                successAddNotification()
+            })
+            .catch(error => {
+                if (error.response.status === 500 || error.response.status === 400) {
+                    console.log(error.message)
+                    errorNotification()
+                }
+            })
+    },
+    putHotel(hotel, setHotels) {
+        return $api.put(ROUTS_API.hotels + `/${hotel.key}`, ({
+            "nameOfHotel": hotel.nameOfHotel,
+            "city": hotel.city,
+            "countOfStars": hotel.countOfStars,
+            "address": hotel.address,
+            "roomCost": hotel.roomCost
+        }))
+            .then(response => {
+                this.getHotels(setHotels)
+            })
+            .catch(error => {
+                if (error.response.status === 500 || error.response.status === 400) {
+                    console.log(error.message)
+                    errorNotification()
+                }
+            })
+    }
+}
+
+export default HotelService;
