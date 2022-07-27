02854040c120cb88869095d02d17d503211173bf
Dana <danaz.radetskaya@gmail.com>
2022-07-27 15:22:11 +0300

    created new file with ticket service

---
+++ src/service/ticket.js
@@ -0,0 +1,52 @@
+import $api from '../http';
+import { ROUTS_API } from "../routs";
+
+import { notification } from "antd";
+import { CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+import axios from "axios";
+
+function successDeleteNotification() {
+    notification.open({
+        message: 'Данные успешно удалены!',
+        icon: <DeleteOutlined style={{color: "green"}}/>
+    })
+}
+
+function successAddNotification() {
+    notification.open({
+        message: 'Данные успешно добавлены!',
+        icon: <CheckCircleOutlined style={{color: "green"}}/>
+    })
+}
+
+function errorNotification() {
+    notification.open({
+        message: 'Что-то пошло не так!',
+        icon: <CloseCircleOutlined style={{color: "red"}}/>
+    });
+}
+
+const TicketService = {
+    getTicketByClientId(setTickets) {
+        return $api.get(ROUTS_API.tickets + `/findTickets?clientId=${5}`)
+            .then(response => {
+                setTickets(response.data);
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    },
+    deleteTicket (key, setTickets, tickets) {
+        return $api.delete(ROUTS_API.tickets + `/${key}`)
+            .then(response => {
+                setTickets(tickets.filter((item) => item.ticketId !== key))
+                successAddNotification()
+            })
+            .catch(error => {
+                console.log(error.message)
+                errorNotification()
+            })
+    }
+}
+
+export default TicketService;
