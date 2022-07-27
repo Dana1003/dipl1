c724dbdb541a166bf40a4076a626c496493fce8a
Dana <danaz.radetskaya@gmail.com>
2022-07-27 16:38:01 +0300

    modified code

--- src/http/index.js
+++ src/http/index.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 import { API_URL } from "../routs";
 
-
 const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL,
     headers: {
         "Access-Control-Allow-Origin": "*",
         "content-type": "application/json",
-
     }
 });
 
