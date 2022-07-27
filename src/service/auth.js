c244c61dd964ff6aee50c5079a7b887a7b45d114
Dana <danaz.radetskaya@gmail.com>
2022-07-27 16:37:43 +0300

    added postUser

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,10 +1,33 @@
 import $api from '../http';
-import {ROUTS_API} from "../routs";
+import { ROUTS_API } from "../routs";
+
+import { notification } from "antd";
+import { CloseCircleOutlined } from "@ant-design/icons";
+
+function errorNotification() {
+    notification.open({
+        message: 'Что-то пошло не так!',
+        icon: <CloseCircleOutlined style={{color: "red"}}/>
+    });
+}
 
 const AuthService = {
-    register(obj){
-        return $api.post(ROUTS_API.getUsers, {obj}).then(res => res.data);
-    },
-};
+    postUser(login, password, navigate) {
+        return $api.post(ROUTS_API.users, {
+            "login": login,
+            "password": password,
+            "role": "User"
+        })
+            .then(response => {
+                return navigate('/userMainPage');
+            })
+            .catch(error => {
+                if (error.response.status === 500 || error.response.status === 400) {
+                    console.log(error.message)
+                    errorNotification()
+                }
+            })
+    }
+}
 
 export default  AuthService;
