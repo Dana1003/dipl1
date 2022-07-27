b2190b635d9ab5b0f53a21454f0ed7c4919ef64e
Dana <danaz.radetskaya@gmail.com>
2022-07-27 16:18:26 +0300

    added putClient

--- src/service/client.js
+++ src/service/client.js
@@ -1,8 +1,9 @@
 import $api from '../http';
-import { ROUTS_API } from "../routs";
+import {ROUTS_API} from "../routs";
 
-import { notification } from "antd";
-import { CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+import {notification} from "antd";
+import {CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined} from "@ant-design/icons";
+import moment from "moment";
 
 function successDeleteNotification() {
     notification.open({
@@ -33,6 +34,35 @@ const ClientService = {
             })
             .catch(error => {
                 console.log(error.message)
+            })
+    },
+    putClient(client, setClient) {
+        return $api.put(ROUTS_API.clientUser + `/${client.clientId}`, ({
+            clientId: client.clientId,
+            passportNumber: client.passportNumber,
+            email: client.email,
+            bithDate: moment(client.bithDate).utcOffset('GMT').format(),
+            userId: client.userId,
+            firstName: client.firstName,
+            lastName: client.lastName,
+            patronymic: client.patronymic,
+            phone: client.phone,
+            login: client.login,
+            password: client.password,
+            role: "User"
+        }))
+            .then(response => {
+                $api.get(ROUTS_API.clients + `/${client.clientId}`)
+                    .then(response => {
+                        setClient(response.data)
+                        successAddNotification()
+                    })
+            })
+            .catch(error => {
+                if (error.response.status === 500 || error.response.status === 400) {
+                    console.log(error.message)
+                    errorNotification()
+                }
             })
     }
 }
