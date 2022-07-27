b5f432f027bc34f3817ade6595eacca29a71e3b8
Dana <danaz.radetskaya@gmail.com>
2022-07-27 15:34:14 +0300

    added postTourHotelToFavourite

--- src/service/tourHotel.js
+++ src/service/tourHotel.js
@@ -3,6 +3,7 @@ import { ROUTS_API } from "../routs";
 
 import { notification } from "antd";
 import { CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+import TourService from "./tour";
 
 function successDeleteNotification() {
     notification.open({
@@ -67,6 +68,38 @@ const TourHotelService = {
             .catch(error => {
                 console.log(error.message)
                 errorNotification()
+            })
+    },
+    postTourHotelToFavourite(selectedTour, selectedHotel, setIsHotelsVisible, setSelectedHotel, setSelectedTour, setTours) {
+        return $api.post(ROUTS_API.tourHotel, ({
+            "tourId": selectedTour.key,
+            "hotelId": selectedHotel.key
+        }))
+            .then(response => {
+                $api.post(ROUTS_API.favourite, ({
+                    "clientId": 5,
+                    "tourHotelId": response.data.tourHotelId,
+                    "status": false
+                }))
+                    .then(response => {
+                        setIsHotelsVisible(false)
+                        setSelectedHotel(null)
+                        setSelectedTour(null)
+                        TourService.getTours(setTours)
+                        successAddNotification()
+                    })
+                    .catch(error => {
+                        if (error.response.status === 500 || error.response.status === 400) {
+                            console.log(error.message)
+                            errorNotification()
+                        }
+                    })
+            })
+            .catch(error => {
+                if (error.response.status === 500 || error.response.status === 400) {
+                    console.log(error.message)
+                    errorNotification()
+                }
             })
     }
 }
